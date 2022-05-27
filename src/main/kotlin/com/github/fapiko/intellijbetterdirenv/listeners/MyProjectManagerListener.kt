77b0a97358a9a135a30bc789f81248d09c259104
GitHub Action <action@github.com>
2022-05-27 20:25:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/fapiko/intellijbetterdirenv/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/fapiko/intellijbetterdirenv/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.fapiko.intellijbetterdirenv.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.fapiko.intellijbetterdirenv.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
